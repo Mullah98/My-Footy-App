6c9c79c12f4617c686acb47c52d0410ece455b4a
Ibrahim <ibz_374@hotmail.com>
2024-08-15 01:14:32 +0100

    Worked on formatting the date property to display the fixtures date to the user more clearly using Intl.DateTimeFormat

--- pages/fixtures.js
+++ pages/fixtures.js
@@ -3,6 +3,7 @@
 import { getFixtures } from "@/utils/apiFootball";
 import { useQuery } from "react-query";
 import "../styling/fixtures.css"
+import Image from "next/image";
 
 export default function Fixtures({ leagueId }) {
     const {data: fixturesData, isLoading} = useQuery(
@@ -15,17 +16,41 @@ export default function Fixtures({ leagueId }) {
         if (!fixturesData) {
             return [];
         }
-        const result = fixturesData;
-        console.log(result[0]);
+
+        const formatter = new Intl.DateTimeFormat("en-GB", {
+            dateStyle: "medium",
+            timeStyle: "short",
+            hour12: true,
+        })    
+
+        const result = fixturesData.map(fixtures => {
+            const fixturesDate = new Date(fixtures.fixture.date);
+            const formattedDate = formatter.format(fixturesDate);
+            
+            return {...fixtures, formattedDate}
+        });
+        // console.log(result);
         return result;
     }
-    const fixtures = filterFixtures(fixturesData);
 
+    const fixtures = filterFixtures(fixturesData);
+    console.log(fixtures[1]);
+    
+    
     return (
         <div className="fixtures-container">
-            <h1>Fixtures</h1>
-        {fixtures.map((fixture, i) => (
-            <h3 key={i}>{fixture.teams.home.name} vs {fixture.teams.away.name}</h3>
+          <h1>Fixtures</h1>
+            {fixtures.map((fixture, i) => (
+          <div className="matches" key={i}>
+          <h4>{fixture.formattedDate}</h4>
+          <h4>Venue: {fixture.fixture.venue.name}</h4>  
+          <h3>
+           <Image src={fixture.teams.home.logo} alt="icon for home team" height={150} width={140} priority={true} />
+           {fixture.teams.home.name} vs {fixture.teams.away.name}
+           <Image src={fixture.teams.away.logo} alt="icon for away team" height={150} width={140} priority={true} />
+           </h3>
+
+          </div>
         ))}
         </div>
     )
