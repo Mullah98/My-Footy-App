cc3dfb24b4c3d7ffb439a6b7d5dc6c4ef07c33ff
Ibrahim <ibz_374@hotmail.com>
2024-08-20 15:22:55 +0100

    Changed getFixtures() endpoint to filter through rounds. New functionality added to allow users to change fixture rounds to render new round of fixtures

--- components/fixtures.js
+++ components/fixtures.js
@@ -5,10 +5,15 @@ import { useQuery } from "react-query";
 import "../styling/fixtures.css"
 import CardSlider from "@/components/cardSlider";
 import {Mosaic} from 'react-loading-indicators';
+import { useEffect, useState } from "react";
 
 export default function Fixtures({ leagueId }) {
+    const [round, setRound] = useState(1);
+    const [manualChangeRound, setManualChangeRound] = useState(false);
+    const maxRounds = 38;
+
     const {data: fixturesData, isLoading} = useQuery(
-    ['fixtures', leagueId], () => getFixtures(leagueId), {
+    ['fixtures', leagueId, round], () => getFixtures(leagueId, round), {
         staleTime: Infinity,
         cacheTime: Infinity,
     });
@@ -32,11 +37,38 @@ export default function Fixtures({ leagueId }) {
             return result;
     }
 
+    const isRoundComplete = (fixturesData) => {
+        return fixturesData && fixturesData.every(fixture => fixture.fixture.status.short === 'FT')
+    }
+
     const fixtures = filterFixtures(fixturesData);
-    
+
+    useEffect(() => {
+        if (isRoundComplete(fixturesData) && !manualChangeRound) {
+            setRound(prevRound => prevRound + 1)
+        }
+    }, [fixturesData, manualChangeRound])
+
+    const handlePrevRound = (e) => {
+        setManualChangeRound(true);
+        setRound(currentRound => currentRound - 1, 1);
+    }
+
+    const handleNextRound = (e) => {
+        setManualChangeRound(true);
+        setRound(currentRound => currentRound + 1, maxRounds);
+    }
+        
     return (
         <div className="fixtures-container">
           <h1>Fixtures</h1>
+          
+          <div className="round-bar">
+            <button className="round-btn" onClick={handlePrevRound} disabled={round === 1}>Prev</button>
+                <span className="round-text">Round {round}</span>
+            <button className="round-btn" onClick={handleNextRound} disabled={round === maxRounds}>Next</button>
+          </div>
+
           {isLoading ? (
             <div className="loading">
             <Mosaic color="#32cd32" size="large" text="Loading" textColor="" />
