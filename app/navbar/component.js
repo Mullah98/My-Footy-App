965e89a65fda2cab07ff8b7735061cce862e83b3
Ibrahim <ibz_374@hotmail.com>
2024-08-23 23:42:55 +0100

    Fixed the search bar for the clubs page. Search results have been reduced to match the popular teams instead of lower league teams

--- app/navbar/component.js
+++ app/navbar/component.js
@@ -1,13 +1,13 @@
 import Link from 'next/link';
-import "./navbar.css"
+import styles from "./navbar.module.css"
 
 export default function Navbar() {
     return (
-        <div className="navbar">
-            <ul>
-                <li><Link href="/standings">Standings</Link></li>
-                <li><Link href="/clubs">Clubs</Link></li>
-                <li><Link href="/players">Players</Link></li>
+        <div className={styles.navbar}>
+            <ul className={styles.ul}>
+                <li className={styles.li}><Link href="/standings">Standings</Link></li>
+                <li className={styles.li}><Link href="/clubs">Clubs</Link></li>
+                <li className={styles.li}><Link href="/players">Players</Link></li>
             </ul>
         </div>
     )
