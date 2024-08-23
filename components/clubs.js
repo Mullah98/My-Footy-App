7ab99fb068b0f26e35c3f0a428656ec107685caf
Ibrahim <ibz_374@hotmail.com>
2024-08-24 00:29:48 +0100

    Working on displaying the information of the clubs

--- components/clubs.js
+++ components/clubs.js
@@ -4,12 +4,13 @@ import { useState } from "react";
 import "../styling/clubs.css"
 import { useQuery } from "react-query";
 import { searchTeam } from "@/utils/apiFootball";
+import Image from "next/image";
 
 
 export default function Clubs({team}) {
     const [club, setClub] = useState('');
-    const nameOfLeague = ['Premier League', 'La Liga', 'Ligue 1', 'Bundesliga', 'Serie A']
-    const countries = ['England', 'Spain', 'France', 'Germany', 'Italy']
+    const [selectedClub, setSelectedClub] = useState(null);
+    const countries = ['England', 'Spain', 'France', 'Germany', 'Italy'];
 
 
     const { data: teamsData, isLoading } = useQuery(['teams', team, club], () => searchTeam(club), {
@@ -30,21 +31,35 @@ export default function Clubs({team}) {
     }
     
     const clubs = filterData(teamsData);
+    console.log(clubs);
+    
+    
 
     return (
         <div className="clubs-container">
             <div className="form">
-                <input type="text"  placeholder="search for team..." value={club} onChange={handleChange}></input> 
-
+                <input type="text" placeholder="search for team..." value={club} onChange={handleChange}></input>
             {clubs && Array.isArray(clubs) && (
-                <ul className="form-ul" key={clubs.id}>
+                <>
+                <ul className="form-ul">
                     {clubs.map((club) => (
-                        <li className="form-li" key={club.team.id}>
+                        <li className="form-li" key={club.team.id} onClick={() => setSelectedClub(club)}>
                             {club.team.name}
                         </li>
                     ))}
                 </ul>
+                </>
             )}
+            
+            {selectedClub && (
+            <div>
+            <h1>{selectedClub.team.name}</h1>
+            <h2>{selectedClub.team.country}</h2>
+            <Image src={selectedClub.team.logo} alt="icons for leagues" height={350} width={300} priority={true} />
+            </div>
+            )}
+
+
             </div>
         </div>
     )
