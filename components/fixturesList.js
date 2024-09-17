fac6d74eea388a1bcd7b4d5d7d549b5171f4d467
Ibrahim <ibz_374@hotmail.com>
2024-09-17 01:05:02 +0100

    Completed the styling for the fixtures.

--- components/fixturesList.js
+++ components/fixturesList.js
@@ -20,24 +20,38 @@ export default function FixturesList({leagueId}) {
         return [];
     }
 
-        const formatter = new Intl.DateTimeFormat("en-GB", {
-            hour: 'numeric',
-            minute: 'numeric',
-            month: 'short',
-            day: 'numeric',
-            hour12: true,
-        })    
+    const dateFormatter = new Intl.DateTimeFormat("en-GB", {
+        month: 'short',
+        day: 'numeric',
+    });
+    
+    const timeFormatter = new Intl.DateTimeFormat("en-GB", {
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true, 
+    });
 
         const result = fixturesList.map(fixtures => {
             const fixturesDate = new Date(fixtures.fixture.date);
-            const formattedDate = formatter.format(fixturesDate);
-            return {...fixtures, formattedDate}
+            const formattedDate = dateFormatter.format(fixturesDate);
+            const formattedTime = timeFormatter.format(fixturesDate)
+            return {...fixtures, formattedDate, formattedTime}
         });
             return result;
     }
 
     const isRoundComplete = (fixturesList) => {
         return fixturesList && fixturesList.every(fixture => fixture.fixture.status.short === 'FT')
+    }
+
+    const handlePrevRound = (e) => {
+        setManualChangeRound(true);
+        setRound(currentRound => currentRound - 1, 1);
+    }
+    
+    const handleNextRound = (e) => {
+        setManualChangeRound(true);
+        setRound(currentRound => currentRound + 1, maxRounds);
     }
 
     
@@ -48,41 +62,48 @@ export default function FixturesList({leagueId}) {
     }, [fixturesList, manualChangeRound])
 
     const fixtureList = filterFixtures(fixturesList)
+    console.log(fixtureList);
+    
 
     return (
         <>
-        {fixtureList && (
-            <div className="fixtures-list">
-                {fixtureList.map((fixture, i) => (
-                    <li key={i} className="fixture">
-                    <div className="fixture-fulldate">
-                        <h2></h2>
-                    </div>
-                    <div className="fixture-teams">
-                    <div className="fixture-team">
-                        <h4>{fixture.teams.home.name}</h4>
-                        <Image src={fixture.teams.home.logo}
-                        alt="team logo"
-                        width={30}
-                        height={30}
-                        priority={true} />
-                    </div>
-                    <div className="fixture-time">
-                        <h4>vs</h4>
-                    </div>
-                    <div className="fixture-team">
-                        <Image src={fixture.teams.away.logo}
-                        alt="team logo"
-                        width={30}
-                        height={30}
-                        priority={true} />
-                        <h4>{fixture.teams.away.name}</h4>
-                    </div>
-                    </div>
-                    </li>
-                ))}
-            </div>
-        )}
+            {fixtureList && (
+                <div className="fixtures-list">
+                <div className="buttons">
+                    <button onClick={handlePrevRound} disabled={round === 1}>previous</button>
+                    <h2>Round {round}</h2>
+                    <button onClick={handleNextRound} disabled={round === maxRounds}>next</button>
+                </div>
+                    {fixtureList.map((fixture, i) => (
+                        <li key={i} className="fixture">
+                            <div className="fixture-teams">
+                                <h4 className="team-name">{fixture.teams.home.name}</h4>
+                                <div className="center-content">
+                                    <Image 
+                                        src={fixture.teams.home.logo} 
+                                        alt="home team logo" 
+                                        width={30} 
+                                        height={30} 
+                                        priority={true} 
+                                    />
+                                    {fixture.fixture.status.elapsed === null ? (
+                                    <h4>{fixture.formattedTime}</h4>
+                                    ) : 
+                                    <h4>{fixture.goals.home} - {fixture.goals.away}</h4>}
+                                    <Image 
+                                        src={fixture.teams.away.logo} 
+                                        alt="away team logo" 
+                                        width={30} 
+                                        height={30} 
+                                        priority={true} 
+                                    />
+                                </div>
+                                <h4 className="team-name">{fixture.teams.away.name}</h4>
+                            </div>
+                        </li>
+                    ))}
+                </div>
+            )}
         </>
     )
 }
